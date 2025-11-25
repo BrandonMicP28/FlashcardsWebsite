9f0649a6149e561efdf03440b3f47479d073cbec
Brandon <brandonphung28@gmail.com>
2025-11-25 00:24:04 -0500

    updated ui

--- database.js
+++ database.js
@@ -11,9 +11,21 @@ const pool = new Pool({
 
 (async () => {
     try {
+        await pool.query(`CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, 
+                                                            google_id TEXT UNIQUE NOT NULL, 
+                                                            email TEXT UNIQUE NOT NULL, 
+                                                            display_name TEXT UNIQUE, 
+                                                            avatar_url TEXT, 
+                                                            time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+                                                            last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`)
+        await pool.query(`CREATE TABLE IF NOT EXISTS decks (id SERIAL PRIMARY KEY, 
+                                                            title TEXT NOT NULL, 
+                                                            user_id INTEGER REFERENCES users(id))`)
         await pool.query(`CREATE TABLE IF NOT EXISTS flashcards (id SERIAL PRIMARY KEY, 
-                                           word TEXT UNIQUE NOT NULL, 
-                                           definition TEXT NOT NULL)`)
+                                                                 word TEXT UNIQUE NOT NULL, 
+                                                                 definition TEXT NOT NULL, 
+                                                                 knowledge_level INTEGER DEFAULT 0, 
+                                                                 deck_id INTEGER REFERENCES decks(id))`)
     } catch (error) {
         console.error("error while creating table:", error)
     }
--- public/client.js
+++ public/client.js
@@ -1,10 +1,10 @@
 const getCardButton = document.getElementById('get-card-button');
-const display = document.getElementById('card-display');
+const flashcardButton = document.getElementById('flashcard-btn');
 
 getCardButton.addEventListener('click', async () => {
     const response = await fetch('http://192.168.1.182:3000/card');
 
     const data = await response.json()
 
-    display.innerText = `Q: ${data.word} \n A: ${data.definition}`
+    flashcardButton.innerText = `Q: ${data.word} \n A: ${data.definition}`
 })
